6d2d117570c56e19d0368bd12e264613e4d76bcf
PatienceIsKeyX <141591785+PatienceIsKeyX@users.noreply.github.com>
2026-01-15 05:18:57 +0100

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,14 +1,9 @@
 package com.example.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+import com.example.addon.modules.AutoMessage;
+import com.example.addon.modules.DiamondHighlighter;
 import com.mojang.logging.LogUtils;
-import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
@@ -16,20 +11,14 @@ import org.slf4j.Logger;
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
 
     @Override
     public void onInitialize() {
         LOG.info("Initializing Meteor Addon Template");
 
-        // Modules
-        Modules.get().add(new ModuleExample());
-
-        // Commands
-        Commands.add(new CommandExample());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
+        // Registering your modules so they appear in the GUI
+        Modules.get().add(new AutoMessage());
+        Modules.get().add(new DiamondHighlighter());
     }
 
     @Override
@@ -41,14 +30,7 @@ public class AddonTemplate extends MeteorAddon {
     public String getPackage() {
         return "com.example.addon";
     }
-
-    @Override
-    public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
-    }
-@Override
-public void onInitialize() {
-    // ... other code ...
+}
 
     // Modules
     Modules.get().add(new AutoMessage());
